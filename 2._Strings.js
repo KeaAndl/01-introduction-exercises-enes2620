51e874a3fb11f2726401345e0bd51e89d65842ff
enes2620 <enestiftikci2620@gmail.com>
2025-01-31 14:56:31 +0100

    Jeg har lavet all opg

--- 2._Strings.js
+++ 2._Strings.js
@@ -1,27 +1,29 @@
 // --------------------------------------
 // Exercise 3 - Add numbers from string to float
 
-const numberOne = "1.10";
-const numberTwo = "2.30";
-
+const numberOne = "1.101";
+const numberTwo = "2.3020";
+const sumOfTwo = parseFloat(numberOne) + parseFloat(numberTwo)
 // add those two numbers and show the result
 // you cannot touch line 1 neither line 2
-
+console.log(sumOfTwo)
 
 // --------------------------------------
 // Exercise 4 - Add the numbers and the total with 2 decimals
 
 const anotherNumberOne = "1.10";
 const anotherNumberTwo = "2.30";
-
-
+const sum = parseFloat(anotherNumberOne) + parseFloat(anotherNumberTwo)
+console.log(sum.toFixed(2))
 // --------------------------------------
 // Exercise 5 - Decimals and average
 
 const one = 10;
 const two = 45;
 const three = 98;
-
+const totalSum = one+two+three
+const avg = totalSum / 3
+console.log(avg.toFixed(5))
 // Show in the console the avg. with 5 decimals
 
 
@@ -31,6 +33,7 @@ const three = 98;
 // Exercise 6 - Get the character by index
 
 const letters = "abc";
+console.log(letters.search("c"))
 // Get me the character "c"
 
 
@@ -40,6 +43,7 @@ const letters = "abc";
 // Exercise 7 - Replace
 
 const fact = "You are learning javascript!";
+console.log(fact.replace("javascript", "Javascript"))
 
 // capitalize the J in Javascript
 
--- 4._Arrays.js
+++ 4._Arrays.js
@@ -5,7 +5,7 @@
 
 const letters = ["a", "b", "c"];
 // show b in the console 
-
+console.log(letters[1])
 
 // --------------------------------------
 // Exercise 2 - Array Positioning
@@ -13,8 +13,10 @@ const letters = ["a", "b", "c"];
 const friends = [];
 
 // What a lonely array. Add at least 3 friend objects to it.  
-
-
+friends.push("Jens")
+friends.push("Yusuf")
+friends.push("Anders")
+console.log(friends)
 // --------------------------------------
 // Exercise 3 - Get the index of first occurance of that value. 
 
@@ -22,7 +24,7 @@ const significantMathNumbers = [0, 2.718, 3.14159, 1729];
 
 // You want to programmatically find where the number 1729 is in the array.
 // programmatically means that no finger counting allowed. There is a method for this (finding index based of value). 
-
+console.log(significantMathNumbers.indexOf(1729))
 
 // --------------------------------------
 // Exercise 4 - Inserting elements
@@ -30,7 +32,9 @@ const significantMathNumbers = [0, 2.718, 3.14159, 1729];
 const diet = ["tomato", "cucumber", "rocket"];
 
 // You are a programmer. In one line (one statement) insert hamburger, soda and pizza between the elements cucumber and rocket
-
+const rocketNumber=diet.indexOf("rocket")+1
+diet.splice(rocketNumber,0,"hamburger","soda","pizza")
+console.log(diet)
 
 
 
@@ -40,7 +44,8 @@ const diet = ["tomato", "cucumber", "rocket"];
 // Remove the LAST element of the array.
 // Don't remove by index. You know in advance that it's the last in the array because you are too full already. 
 
-
+diet.pop()
+console.log(diet)
 
 
 
@@ -49,17 +54,17 @@ const diet = ["tomato", "cucumber", "rocket"];
 
 // You really like your daily diet from last exercise. Copy it to a new array called dinnerTray so you can give it to a friend.  
 
-
-
+const dinnerTray = [...diet]
+dinnerTray.push("MilkShake")
+console.log(dinnerTray)
 // --------------------------------------
 // Exercise 7 - For loop
 
 const lettersExpanded = ["a", "b", "c", "d", "e", "f", "g", "h"];
 
 // log every second char in the array starting from b
-
-
-
+const everyOther = lettersExpanded.filter((_,index)=>index % 2 === 1)
+console.log(everyOther)
 // --------------------------------------
 // Exercise 8 - For loop and if statement
 
@@ -69,7 +74,8 @@ const discardedNumbers = [];
 
 // log the element if the number is above 6 or below 0
 // else push them to the array discardedNumbers
-
+numbers.forEach(number=>number<6 ? console.log("These Are The Passed Numbers:",number): discardedNumbers.push(number))
+console.log("This Is The Discarded Numbers:",discardedNumbers)
 // --------------------------------------
 
 
